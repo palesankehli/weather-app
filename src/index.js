6e55eba31fcfe571242ea738cca9fdd143365da5
Palesa Nkehli <93646366+palesankehli@users.noreply.github.com>
2024-05-08 01:13:02 +0200

    added wind and humidity realtime values

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+function giveWeather(response) {
+  let updateTemperature = document.querySelector(".temperature-value");
+  updateTemperature.innerHTML = Math.round(response.data.temperature.current);
+  let updateHumidity = document.querySelector(".humidity-value");
+  updateHumidity.innerHTML = response.data.temperature.humidity;
+  let updateWind = document.querySelector(".wind-value");
+  updateWind.innerHTML = response.data.wind.speed;
+  let weatherCondition = document.querySelector(".condition");
+  weatherCondition.innerHTML = response.data.condition.description;
+}
+
+function giveCityElement(city) {
+  let apiKey = "a04o1a335def673e75084b5bt0de68b5";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=" +
+    city +
+    "&key=" +
+    apiKey;
+  axios.get(apiUrl).then(giveWeather);
+}
+
 function searchBegin(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#city-search-box");
@@ -6,11 +27,8 @@ function searchBegin(event) {
     searchInputElement.value.charAt(0).toUpperCase() +
     searchInputElement.value.slice(1);
   cityELement.innerHTML = capitalizedValue;
+  giveCityElement(capitalizedValue);
 }
-
-let apiKey = "a04o1a335def673e75084b5bt0de68b5";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}";
 
 let searchCityElement = document.querySelector("#city-search");
 searchCityElement.addEventListener("submit", searchBegin);
