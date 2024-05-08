de02710bac1cb6a412dfabdb88510472b6d266d6
Palesa Nkehli <93646366+palesankehli@users.noreply.github.com>
2024-05-08 02:22:53 +0200

    added if statement for minutes

--- src/index.js
+++ src/index.js
@@ -28,6 +28,14 @@ function formatDate(date) {
   let day = days[date.getDay()];
 
   return day + " " + hours + ":" + minutes;
+
+  if (minutes > 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (hours > 10) {
+    hours = "0" + hours;
+  }
 }
 
 function giveCityElement(city) {
